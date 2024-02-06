06caf5ec9bb1077a7a25d6e089595fb2569e42ee
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-06 19:50:33 +0100

    Recovering the file from formData

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,5 +1,10 @@
 import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async({ request }) => {
+  const formData = await request.formData()
+  const file = formData.get('file') as File
+
+  console.log(file)
+
   return new Response('Hello World!')
 }
