d8b4a1d4d1bdaaa6871fd14165d94e1042b8de65
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-06 18:45:29 +0100

    fix module.export & test flowbite alert

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,1 +1,8 @@
-<h1>App</h1>
+<script>
+  import { Alert } from 'flowbite-svelte';
+</script>
+
+<Alert>
+  <span class="font-medium">Default alert!</span>
+  Change a few things up and try submitting again.
+</Alert>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -29,5 +29,3 @@ export default {
     }
   }
 };
-
-module.exports = config;
