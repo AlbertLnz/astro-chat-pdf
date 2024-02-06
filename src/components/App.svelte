8ed3b79c269f3d561c06b1867a2402a6c74a63a7
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-06 18:58:57 +0100

    store.ts in Svelte & conditional looking the store

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,8 +1,27 @@
 <script>
-  import { Alert } from 'flowbite-svelte';
+  import { Alert } from 'flowbite-svelte'
+  import { APP_STATUS, appStatus } from '../store.ts'
 </script>
 
-<Alert>
-  <span class="font-medium">Default alert!</span>
-  Change a few things up and try submitting again.
-</Alert>
+{#if $appStatus === APP_STATUS.INIT}
+  <p>La página incial</p>
+
+{:else if $appStatus === APP_STATUS.LOADING}
+  <p>Loading...</p>
+
+{:else if $appStatus === APP_STATUS.CHAT_MODE}
+  <p>Chatting</p>
+
+{:else if $appStatus === APP_STATUS.ERROR}
+  <Alert>
+    <span class="font-medium">Error!</span>
+    ¡La aplicación tiene algún tipo de error!
+  </Alert>
+
+{:else}
+  <Alert color="red">
+    <span class="font-medium">Acción no reconocida!</span>
+    ¡Este estado de la aplicación no está reconocido!
+  </Alert>
+
+{/if}
---
+++ src/store.ts
@@ -0,0 +1,10 @@
+import { writable } from "svelte/store";
+
+export const APP_STATUS = {
+  INIT: 0,
+  LOADING: 1,
+  CHAT_MODE: 2,
+  ERROR: -1
+}
+
+export const appStatus = writable(APP_STATUS.INIT)
