21aeeda81645f7bd3edadb894cef18fc6dfe82e3
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-06 20:08:13 +0100

    Configuration of Cloudinary

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,4 +1,11 @@
 import type { APIRoute } from "astro";
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({ 
+  cloud_name: 'dxgurhzge', 
+  api_key: '945854376749222', 
+  api_secret: import.meta.env.env.API_SECRET_CLOUDINARY
+});
 
 export const POST: APIRoute = async({ request }) => {
   const formData = await request.formData()
