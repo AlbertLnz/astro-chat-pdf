c942e636b253ba9ef5c1001119d6a547d5c4af5b
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-06 20:13:00 +0100

    Creating the function to upload the ArrayBuffer of the file

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -7,6 +7,17 @@ cloudinary.config({
   api_secret: import.meta.env.env.API_SECRET_CLOUDINARY
 });
 
+const uploadStream = async (buffer: Uint8Array, options: {
+  folder: string
+}) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.uploadStream(options, result) => {
+      if (result) return resolve(result)
+      reject(error)
+    }
+  }).end(buffer)
+}
+
 export const POST: APIRoute = async({ request }) => {
   const formData = await request.formData()
   const file = formData.get('file') as File
