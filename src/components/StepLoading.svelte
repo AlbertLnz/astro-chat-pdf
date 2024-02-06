121567094b4ceebdf517d064c303867412d57279
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-06 19:31:06 +0100

    Styling the Spinner container

--- src/components/StepLoading.svelte
+++ src/components/StepLoading.svelte
@@ -2,4 +2,7 @@
   import { Spinner } from 'flowbite-svelte';
 </script>
 
-<Spinner />
+<div class='flex flex-col justify-center items-center gap-y-2'>
+  <Spinner />
+  <p class='opacity-75'>Subiendo el archivo y extrayendo el texto</p>
+</div>
