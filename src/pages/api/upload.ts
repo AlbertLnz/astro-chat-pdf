14f57c2e2b579b56ee663fd38479aef4d8d0f309
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-06 20:34:51 +0100

    Fixing some errors & upload PDF to Cloudinary

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,30 +1,43 @@
 import type { APIRoute } from "astro";
-import { v2 as cloudinary } from 'cloudinary'
+import { v2 as cloudinary, type UploadApiResponse } from 'cloudinary'
 
 cloudinary.config({ 
   cloud_name: 'dxgurhzge', 
   api_key: '945854376749222', 
-  api_secret: import.meta.env.env.API_SECRET_CLOUDINARY
+  api_secret: import.meta.env.API_SECRET_CLOUDINARY
 });
 
 const uploadStream = async (buffer: Uint8Array, options: {
   folder: string
-}) => {
+}): Promise<UploadApiResponse> => {
   return new Promise((resolve, reject) => {
-    cloudinary.uploader.uploadStream(options, result) => {
+    cloudinary.uploader.upload_stream(options, (error, result) => {
       if (result) return resolve(result)
       reject(error)
-    }
-  }).end(buffer)
+    }).end(buffer)
+  })
 }
 
 export const POST: APIRoute = async({ request }) => {
   const formData = await request.formData()
   const file = formData.get('file') as File
 
-  console.log(file)
+  if(file == null) return new Response('No file found', { status: 400 })
+  // console.log(file)
 
-  await new Promise((resolve) => setTimeout(resolve, 3000)) // Simulating a deloy of 3s
+  const arrayBuffer = await file.arrayBuffer()
+  const unit8Array = new Uint8Array(arrayBuffer)
 
-  return new Response('Hello World!')
+  const result = await uploadStream(unit8Array, {
+    folder: 'pdf'
+  })
+
+  const { asset_id, url, pages } = result
+
+  // console.log(result)
+  // await new Promise((resolve) => setTimeout(resolve, 3000)) // Simulating a deloy of 3s
+
+  return new Response(JSON.stringify({
+    id, url, pages
+  }))
 }
